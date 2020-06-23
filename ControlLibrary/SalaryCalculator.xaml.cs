b036d27a9104b7a2543ee4250b91c7e2e85dac06
thr828 <592040808@qq.com>
2020-06-23 22:07:29 +0800

    2

---
+++ ControlLibrary/SalaryCalculator.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ControlLibrary
+{
+    /// <summary>
+    /// 用户控件
+    /// </summary>
+    public partial class SalaryCalculator : UserControl
+    {
+        public SalaryCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            this.textBox2.Text = (int.Parse(this.textBox.Text) + int.Parse(this.textBox1.Text)).ToString();
+        }
+    }
+}
--- WpfDemo/MainWindow.xaml.cs
+++ WpfDemo/MainWindow.xaml.cs
@@ -10,19 +10,34 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WpfDemo
 {
     /// <summary>
-    /// MainWindow.xaml 的交互逻辑
+    /// Window1.xaml 的交互逻辑
     /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            //方法2
+            this.Button1.Click += Button1_Click;
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// 方法1
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button1_OnClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("HEELO!");
         }
     }
 }
