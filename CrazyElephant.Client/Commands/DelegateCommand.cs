5bef7e0e6b44932d9c6c72285c6cd66e21ee7203
thr828 <592040808@qq.com>
2020-11-28 22:10:17 +0800

    增加注释

--- CrazyElephant.Client/Commands/DelegateCommand.cs
+++ CrazyElephant.Client/Commands/DelegateCommand.cs
@@ -7,12 +7,17 @@ using System.Windows.Input;
 
 namespace razyElephant.Client.Models
 {
+    /// <summary>
+    /// 单向的命令属性
+    /// </summary>
     public class DelegateCommand : ICommand
     {
-        public event EventHandler CanExecuteChanged;
+       
 
         public Action<object> ExecuteAction { get; set; }
         public Func<object,bool> CanExecuteFunc { get; set; }
+
+        public event EventHandler CanExecuteChanged;
 
         public bool CanExecute(object parameter)
         {
